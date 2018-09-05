90e7e33a9cd6fc8e35e89f57538808d6500f5fb4
brooky56 <aidarahmetshin56@gmail.com>
2018-09-05 16:53:33 +0300

    week3 is ready

---
+++ week3/main.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Elem{
+	int data;
+	struct Elem* next;
+}Node;
+
+typedef Node* list;
+
+list addToList(list a,int num)
+{
+
+	list step, temp;
+	int i=0;
+
+	if(a==NULL)
+	{
+		a = (list)malloc(sizeof(Node));
+		a->data = num;
+		a->next = NULL;
+	}
+	else{
+		step = a;
+
+		while(step->next!=NULL)
+		{
+			step = step->next;
+		}
+
+		temp = (list)malloc(sizeof(Node));
+		temp->data = num;
+		temp->next = NULL;
+		step->next = temp;
+	}
+	return a;
+}
+
+list deleteFromList(list a,int pos)
+{
+	int i=0;
+	list temp, step;
+
+	if(a!=NULL)
+	{
+		step = a;
+
+		if(pos == 0)
+		{
+			a = a->next;
+			step->next = NULL;
+			free(step);
+		}
+
+		else{
+			while(i++!=pos-1)
+			step = step->next;
+			temp = step->next;
+			step->next = temp->next;
+			temp->next = NULL;
+			free(temp);
+		}
+	}
+	return a;
+}
+
+void printList(list a)
+{
+	list temp = a;
+
+	printf("List contains following elements : \n");
+
+	while(temp!=NULL)
+	{
+		printf("%d ",temp->data);
+		temp = temp->next;
+	}
+}
+
+void exer1(){
+
+	int* pc;
+	int c;
+	c=22;
+	printf("Address of c:%d\n",&c);
+	printf("Value of c:%d\n\n",c);
+	pc=&c;
+	printf("Address of pointer pc:%d\n",pc);
+	printf("Content of pointer pc:%d\n\n",*pc);
+	c=11;
+	printf("Address of pointer pc:%d\n",pc);
+	printf("Content of pointer pc:%d\n\n",*pc);
+	*pc=2;
+	printf("Address of c:%d\n",&c);
+	printf("Value of c:%d\n\n",c);
+
+}
+
+void bubble_sort(int *arr, int size)
+{
+	     int t;
+	     for (int i = 0; i < size-1; i++)
+	     {
+	 	for(int j = 0; j < size - i - 1; j++)
+	 	{
+			if(arr[j]>arr[j+1])
+			{
+				t = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = t;
+			}
+		}
+	     }
+}
+
+void exer2()
+{
+	int n;
+	printf("Введите размер массива: ");
+  	scanf("%d", &n);
+	int a[n];
+	for(int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	bubble_sort(a, n);
+	for(int j = 0; j< n; j++)
+		printf("%d%s", a[j], j == n ? "\n":" ");
+}
+
+void exer3()
+{
+	list a = NULL;
+	int i, size;
+	scanf("%d", &size);
+	int arr[size];
+	for(int i = 0; i<size;i++)
+		scanf("%d",&arr[i]);
+	if(size == 1)
+		printf("Only one element in the list",arr[0]);
+	else{
+		for(i=0;i<size;i++)
+			a = addToList(a,arr[i]);
+		printList(a);
+
+		do{
+			printf("\nEnter position of element to be removed (0-%d) : ", size-1);
+
+			scanf("%d",&i);
+
+			if(i<size){
+				a = deleteFromList(a,i);
+				printList(a);
+			}
+		}while(i>size);
+	}
+}
+
+int main()
+{
+	//exer1();
+	//exer2();
+	//exer3();
+	return 0;
+}
